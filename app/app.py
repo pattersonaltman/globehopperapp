394661fcc9ad8a1b128b8ce05fc86b847f83ec33
pattersonaltman <pattersonaltman@yahoo.com>
2023-03-08 11:30:11 -0500

    added but commented out flask_wtf.csrf CSRFProtect

--- app/app.py
+++ app/app.py
@@ -3,12 +3,12 @@
 
 from flask import Flask, request, jsonify
 import country
-from flask_wtf.csrf import CSRFProtect
+# from flask_wtf.csrf import CSRFProtect  #pip install flask_wtf
 
 #Using Flash framework
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app)
+# csrf = CSRFProtect()
+# csrf.init_app(app)
 
 #Create - POST API
 @app.route('/countries', methods=['POST'])
